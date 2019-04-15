c37a74c6c0f79cfd90de15d8f6286df7f17a08e0
Geisson Carlos <geissoncr@gmail.com>
2019-04-15 02:47:11 -0300

    Criando a estrutura do blog

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
 import './App.css';
-
+// import logo from "./content/medtecnologia-logo.svg";
 class App extends Component {
+
+  resultados() {
+    return (
+      <div>
+        <p>Geisson</p>
+      </div>)
+  }
+
+  informacoes() {
+    return (
+      <div>
+        <p>Informacoes da Barra Lateral</p>
+        <div>
+          <p>Pesquisa</p>
+        </div>
+        <div>
+          <p>Formulario</p>
+        </div>
+        <div>
+          <p>Populares</p>
+        </div>
+      </div>
+    )
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Primeiros testes do #Grupo_9.
-          </p>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          {/* <img src={logo} className="App-logo" alt="Texto de teste" /> */}
+          <p className="logo">MedTecNologia</p>
+
         </header>
+        <div className="App-body">
+          <div className="App-posts">
+            {this.resultados()}
+          </div>
+          <div className="App-informacoes">
+            {this.informacoes()}
+          </div>
+        </div>
+        <footer className="App-footer">
+          <p>Parte de Baixo</p>
+        </footer>
       </div>
     );
   }
