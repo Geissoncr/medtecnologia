4bc1ef4aafd18eaceb8f0461b2505f4496a5c62d
brunocdsr <49727685+brunocdsr@users.noreply.github.com>
2019-04-17 15:18:20 -0300

    Update data.js
    1 Post

--- src/component/data.js
+++ src/component/data.js
@@ -7,7 +7,9 @@ export default {
         image: "teste.jpg",
         imageAlt: "mensagem Alternativa da IMagem",
         summary:
-          "Create React App is a great tool that lets you start a new React application very easily. There are some limitations though that you need to be aware of.",
+          "Hoje em dia, a tecnologia está presente em quase todas as nossas atividades. Para nos conectarmos com pessoas distantes, controlar nossos horários, chegar aos nossos destinos, fazer compras, assistir filmes, etc. Entretanto, a tecnologia não possui apenas funcionalidades para lazer, mas também para auxílio na gestão de empresas, através de softwares e app’s que possibilitam a organização dos recursos de uma organização. Dentre essas organizações, a gestão de hospitais e outras instituições da área da saúde."
+"Quando falamos sobre a gestão de hospitais (e empresas num geral), logo pensamos em gerir custos, recursos, escala de funcionários, etc. Porém, esquecemos de uma parte primordial a ser controlada: os medicamentos. Estes produtos precisam de muito cuidado ao serem armazenados, para que não estraguem e, consequentemente, prejudiquem a saúde da pessoa que fizer seu uso. Alguns medicamentos, inclusive, necessitam de um armazenamento especial, pois necessitam de refrigeração para manter sua qualidade. Por isso, a grande importância da tecnologia: garantir a maior eficiência no mapeamento térmico na armazenagem."
+"De acordo com o site da BBC, em 2017 o SUS jogou fora R$ 16 milhões em medicamentos de alto custo e um dos principais motivos foi a armazenagem incorreta, que resultou na perda da validade dos remédios. Portanto, após a escolha da ferramenta tecnológica adequada, a empresa irá perceber uma redução nos desperdícios, otimização no trabalho e redução de custos (visto que não será mais necessário uma pessoas 100% dedicada ao monitoramento) e, acima de tudo, assegurar que todos os pacientes tenham acesso a um tratamento seguro e de qualidade."",
       },
       "post-de-teste2": {
         date: "1958-04-22",
